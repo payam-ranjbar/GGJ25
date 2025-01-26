006403b4726a2ad7a8d74bbd90693978d03a7934
payam-ranjbar <payam.ranjbar@live.com>
2025-01-25 17:21:12 -0700

    UI pages

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager: MonoBehaviour
 {
     [SerializeField] private Animator playerHUDAnimator;
+    [SerializeField] private Animator startScreenAnimator;
+    [SerializeField] private Animator endGameAnimator;
+    [SerializeField] private Text endGameText;
 
     public static UIManager Instance { get; private set; }
 
@@ -29,4 +33,26 @@ public class UIManager: MonoBehaviour
     {
         playerHUDAnimator.SetTrigger("Hide");
     }
+
+    public void HideStartScreen()
+    {
+        startScreenAnimator.SetTrigger("Hide");
+    }
+
+    public void ShowStartScreen()
+    {
+        startScreenAnimator.SetTrigger("Show");
+    }
+
+
+    public void ShowEndGame(string winnerName)
+    {
+        endGameText.text = $"{winnerName} has won!";
+        endGameAnimator.SetTrigger("Show");
+    }
+    public void HideEndGame()
+    {
+        endGameAnimator.SetTrigger("Hide");
+    }
+    
 }
