78213c41f2ee55cf58afbcaa2ff8da2df3d98202
Mohammad Fakhreddin <mohammad.fakhreddin@gmail.com>
2025-01-26 12:42:51 -0700

    Lava is complete

--- Assets/Scripts/PlayerSpawnManager.cs
+++ Assets/Scripts/PlayerSpawnManager.cs
@@ -40,4 +40,9 @@ public class PlayerSpawnManager : MonoBehaviour
         _lava.Activate();
     }
 
+    public void OnPlayerDeath(int index)
+    {
+        Debug.Log($"Player died {index}");
+    }
+
 }
