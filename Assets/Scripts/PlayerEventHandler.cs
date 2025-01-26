4a5de049b3bab6ea52b33a43809da5416642767a
payam-ranjbar <payam.ranjbar@live.com>
2025-01-26 10:26:49 -0700

    Audio Manager

---
+++ Assets/Scripts/PlayerEventHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerEventHandler : MonoBehaviour
+{
+    public static PlayerEventHandler Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public event Action OnJump;
+    public event Action OnAttack;
+    public event Action OnInteract;
+
+    public void TriggerJump()
+    {
+        OnJump?.Invoke();
+    }
+
+    public void TriggerAttack()
+    {
+        OnAttack?.Invoke();
+    }
+
+    public void TriggerInteract()
+    {
+        OnInteract?.Invoke();
+    }
+}
