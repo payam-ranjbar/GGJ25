aedca84d09d13c84cf24e970a765039483451433
Erik Biederstadt <biederstadterik@gmail.com>
2025-01-26 13:47:19 -0700

    Change how the birds are spawned
    This is much less chaos

--- Assets/BirdHazardManager.cs
+++ Assets/BirdHazardManager.cs
@@ -6,11 +6,20 @@ public class BirdHazardManager : MonoBehaviour
 {
     [Header("References")]
     public BirdSpawner[] spawners;
+
+    [Header("Variables")]
     public bool spawnBirds = false;
+    public float minSpeed;
+    public float maxSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        foreach (var spawner in spawners)
+        {
+            spawner.minSpeed = minSpeed;
+            spawner.maxSpeed = maxSpeed;
+        }
     }
 
     // Update is called once per frame
